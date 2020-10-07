a54236aec31066f9854a88cbd15c5910466c2ab3
j3ffh95 <j3ffh95@gmail.com>
2020-10-07 09:49:58 -0400

    Created event listener when mouse moves in the container

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const container = document.getElementById("wrapper"),
   img = document.querySelector("img");
+
+container.addEventListener("mousemove", (e) => {
+  const x = e.clientX - e.target.offsetLeft;
+  const y = e.clientY - e.target.offsetTop;
+
+  // console.log(x, y);
+});
